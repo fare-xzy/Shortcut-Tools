db40430dd01b2a19f9b66fa547ee83c2fec05d7e
GitHub Action <action@github.com>
2023-12-25 06:52:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/farexzy/shortcuttools/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/farexzy/shortcuttools/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.farexzy.shortcuttools.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
